e361346f7dd0ad53328b14d07b7585c08c70c823
PremiumGamer69 <121114496+PremiumGamer69@users.noreply.github.com>
2022-12-20 16:46:54 +0100

    Add files via upload

---
+++ 911/fxmanifest.lua
@@ -0,0 +1,12 @@
+fx_version 'cerulean'
+games { 'gta5' }
+version '1.0.0'
+author 'Premium__Gamer aka PremiumGamer69'
+
+client_scripts {
+    'client.lua'
+}
+
+server_scripts {
+    'server.lua'
+}
